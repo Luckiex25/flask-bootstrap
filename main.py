20e5b631ad6026610092236f722c208510467ee7
Luckiex <std.67122420305@ubru.ac.th>
2026-01-13 15:09:22 +0700

    fix sreach car

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'khiopgfdfghj;oioiouiy'
-
+# app.secret_key = b'khiopgfdfghj;oioiouiy'
 
 cars = [
   {'id':1, 'brand':'Toyota', 'model':'Yaris Ativ', 'year':2024, 'price': 560000},
@@ -17,20 +17,30 @@ def index():
 @app.route('/cars', methods=['GET', 'POST'])
 def show_cars():
   if request.method == 'POST':
-    brand = request.form['brand']
-    # print(brand)
+    brand = request.form.get('brand', '').strip()
+
+    if brand == '':
+      return render_template(
+        'cars/cars.html',
+        title='Show All Cars Page',
+        cars=cars
+      )
     tmp_cars = []
     for car in cars:
-      # print(car)
-      if brand in car['brand']:
+      if brand.lower() in car['brand'].lower():
         tmp_cars.append(car)
-    return render_template('cars/cars.html',
-                         title='Show Cars by Brand Page',
-                         cars=tmp_cars)
-  
-  return render_template('cars/cars.html',
-                         title='Show All Cars Page',
-                         cars=cars)
+
+    return render_template(
+      'cars/cars.html',
+      title='Show Cars by Brand Page',
+      cars=tmp_cars
+    )
+
+  return render_template(
+    'cars/cars.html',
+    title='Show All Cars Page',
+    cars=cars
+  )
 
 @app.route('/cars/new', methods=['GET', 'POST'])
 def new_car():
